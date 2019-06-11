dc0345252691f413154529e7d1a249dee52fe747
Matthew Scott <matthew.s@havealook.com.au>
2019-06-11 12:02:52 +1000

    minor security update, securing index and /

--- resourcemanager/src/main/java/com/resourcemanager/config/SpringSecurityConfig.java
+++ resourcemanager/src/main/java/com/resourcemanager/config/SpringSecurityConfig.java
@@ -35,7 +35,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 			.antMatchers("/resources/**").permitAll()
-			.antMatchers("/", "/index").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
