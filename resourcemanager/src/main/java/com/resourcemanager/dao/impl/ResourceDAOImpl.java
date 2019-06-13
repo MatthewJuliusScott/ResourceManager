3839d89e32ef65a29a714dd79b6cfff1d0007cab
Matthew <matthewjuliusscott@gmail.com>
2019-06-13 18:57:51 +1000

    removing meta model

--- resourcemanager/src/main/java/com/resourcemanager/dao/impl/ResourceDAOImpl.java
+++ resourcemanager/src/main/java/com/resourcemanager/dao/impl/ResourceDAOImpl.java
@@ -23,9 +23,7 @@ import org.springframework.stereotype.Repository;
 
 import com.resourcemanager.dao.ResourceDAO;
 import com.resourcemanager.model.Allocation;
-import com.resourcemanager.model.Allocation_;
 import com.resourcemanager.model.Resource;
-import com.resourcemanager.model.Resource_;
 
 @Repository
 public class ResourceDAOImpl implements ResourceDAO {
@@ -87,19 +85,19 @@ public class ResourceDAOImpl implements ResourceDAO {
 			// get resources that have the skill
 			// and have greater than or equal to hours
 			Predicate skillIdMatch = builder.isMember(skillId, root.get("skills"));
-			Predicate greaterThanOrEqualHours = builder.ge(root.get(Resource_.hours), hours);
+			Predicate greaterThanOrEqualHours = builder.ge(root.get("hours"), hours);
 			
 			// create a subquery to get resources that have allocations in the time period specified between startDate and endDate
 			Subquery<Long> sub = query.subquery(Long.class);
 			Root<Allocation> subRoot = sub.from(Allocation.class);
-			Join<Allocation, Resource> allocations = subRoot.join(Allocation_.resource, JoinType.INNER);
-			Predicate startNoAllocationsInTimePeriod = builder.between(subRoot.get(Allocation_.startDate), startDate, endDate);
-			Predicate endNoAllocationsInTimePeriod = builder.between(subRoot.get(Allocation_.endDate), startDate, endDate);
-			sub.select(allocations.get(Resource_.id));
+			Join<Allocation, Resource> allocations = subRoot.join("resource", JoinType.INNER);
+			Predicate startNoAllocationsInTimePeriod = builder.between(subRoot.get("startDate"), startDate, endDate);
+			Predicate endNoAllocationsInTimePeriod = builder.between(subRoot.get("endDate"), startDate, endDate);
+			sub.select(allocations.get("id"));
 			sub.where(builder.and(startNoAllocationsInTimePeriod, endNoAllocationsInTimePeriod));
 			
 			// exclude resources who have allocations during the specified time period 
-			Predicate priorAllocations = root.get(Resource_.id).in(sub).not();
+			Predicate priorAllocations = root.get("id").in(sub).not();
 					
 			query.select(root).where(builder.and(skillIdMatch, greaterThanOrEqualHours, priorAllocations));
 			query.orderBy(builder.desc(root.get("hours")));
