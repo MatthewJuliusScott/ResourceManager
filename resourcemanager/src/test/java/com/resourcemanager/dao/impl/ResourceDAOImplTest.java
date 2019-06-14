77ccab46714693e429d4e861d00f4ac6669c53a2
Matthew Scott <matthew.s@havealook.com.au>
2019-06-14 12:06:23 +1000

    creating test properties, udpated resource dao test, not working still

--- resourcemanager/src/test/java/com/resourcemanager/dao/impl/ResourceDAOImplTest.java
+++ resourcemanager/src/test/java/com/resourcemanager/dao/impl/ResourceDAOImplTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +14,7 @@ import com.resourcemanager.model.Resource;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase
 public class ResourceDAOImplTest {
 
 	@Autowired
