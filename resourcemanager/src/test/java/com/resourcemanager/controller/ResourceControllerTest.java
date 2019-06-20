bbc4f0c2317b88dfcc6fa6235355fe5396a5e862
Matthew Scott <matthew.s@havealook.com.au>
2019-06-20 11:57:31 +1000

    got first unit test working, was an issue with the pom file. I had
    accidently included multiple copies of the same jar

--- resourcemanager/src/test/java/com/resourcemanager/controller/ResourceControllerTest.java
+++ resourcemanager/src/test/java/com/resourcemanager/controller/ResourceControllerTest.java
@@ -1,6 +1,7 @@
 package com.resourcemanager.controller;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,33 +9,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.resourcemanager.model.Resource;
 import com.resourcemanager.service.ResourceService;
 import com.resourcemanager.service.SkillService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ResourceController.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ResourceControllerTest {
 
 	@Autowired
-	private MockMvc			mvc;
+	private WebApplicationContext	context;
+
+	private MockMvc					mvc;
 
 	@MockBean
-	private ResourceService	resourceService;
+	private ResourceService			resourceService;
 
 	@MockBean
-	private SkillService	skillService;
+	private SkillService			skillService;
 
 	@Test
+	@WithMockUser(username = "user", password = "password", roles = "ADMIN")
 	public void givenResources_whenGetResources_thenReturnJsonArray()
 		throws Exception {
 
@@ -49,5 +58,13 @@ public class ResourceControllerTest {
 			.contentType(MediaType.TEXT_HTML))
 			.andExpect(status().isOk())
 			.andExpect(forwardedUrl("/WEB-INF/views/resources.jsp"));
+	}
+
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders
+			.webAppContextSetup(context)
+			.apply(springSecurity())
+			.build();
 	}
 }
