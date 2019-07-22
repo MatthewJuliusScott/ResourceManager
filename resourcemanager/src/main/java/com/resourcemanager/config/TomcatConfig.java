ae383c740e441f7b3169c0085cc3865985774f20
Matthew <matthewjuliusscott@gmail.com>
2019-07-22 21:00:19 +1000

    added tomcat config

---
+++ resourcemanager/src/main/java/com/resourcemanager/config/TomcatConfig.java
@@ -0,0 +1,26 @@
+
+package com.resourcemanager.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(8009);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("https");
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+
+		return tomcat;
+	}
+
+}
