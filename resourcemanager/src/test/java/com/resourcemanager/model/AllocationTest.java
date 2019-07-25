9ceaa97f0b0403b4cf1f5acca2c2fd295ecb9351
Matthew Scott <matthew.s@havealook.com.au>
2019-07-25 15:38:14 +1000

    updated allocation and resource unit tests, coverage 91.8%

--- resourcemanager/src/test/java/com/resourcemanager/model/AllocationTest.java
+++ resourcemanager/src/test/java/com/resourcemanager/model/AllocationTest.java
@@ -2,6 +2,8 @@ package com.resourcemanager.model;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 import org.junit.Test;
@@ -11,6 +13,13 @@ public class AllocationTest {
 	@Test
 	public void testAllocation() throws Exception {
 		Allocation allocation = new Allocation();
+		assertTrue(allocation != null);
+	}
+
+	@Test
+	public void testAllocationLongProjectSkillLocalDateLocalDateIntResource() throws Exception {
+		Allocation allocation =
+			new Allocation(1L, new Project(), new Skill(), LocalDate.now(), LocalDate.now().plusDays(7), 40, new Resource());
 		assertTrue(allocation != null);
 	}
 
@@ -25,13 +34,13 @@ public class AllocationTest {
 	@Test
 	public void testClone() throws Exception {
 		Allocation expected = new Allocation();
-		expected.setId(new Random().nextLong());
-
-		Resource resource = new Resource();
-		resource.setId(new Random().nextLong());
-
-		Allocation allocation = new Allocation();
-		allocation.setId(new Random().nextLong());
+		expected.setId(1L);
+		expected.setHours(40);
+		expected.setStartDate(LocalDate.now());
+		expected.setEndDate(LocalDate.now().plusDays(7));
+		expected.setProject(new Project());
+		expected.setResource(new Resource());
+		expected.setSkill(new Skill());
 
 		Allocation actual = (Allocation) expected.clone();
 		assertTrue(expected.equals(actual));
@@ -40,16 +49,35 @@ public class AllocationTest {
 	@Test
 	public void testEquals() throws Exception {
 		Resource resource = new Resource();
-
 		Long resourceId = new Random().nextLong();
 		resource.setId(resourceId);
+
+		Project project = new Project();
+		Long projectId = new Random().nextLong();
+		project.setId(projectId);
+
+		Skill skill = new Skill();
+		Long skillId = new Random().nextLong();
+		skill.setId(skillId);
 
 		Long allocationId = new Random().nextLong();
 		Allocation expected = new Allocation();
 		expected.setId(allocationId);
+		expected.setHours(40);
+		expected.setStartDate(LocalDate.now());
+		expected.setEndDate(LocalDate.now().plusDays(7));
+		expected.setProject(project);
+		expected.setResource(resource);
+		expected.setSkill(skill);
 
 		Allocation actual = new Allocation();
 		actual.setId(allocationId);
+		actual.setHours(40);
+		actual.setStartDate(LocalDate.now());
+		actual.setEndDate(LocalDate.now().plusDays(7));
+		actual.setProject(project);
+		actual.setResource(resource);
+		actual.setSkill(skill);
 
 		assertTrue(expected.equals(actual));
 	}
@@ -61,12 +89,110 @@ public class AllocationTest {
 	}
 
 	@Test
+	public void testGetEndDate() throws Exception {
+		Allocation allocation = new Allocation();
+		LocalDate expected = LocalDate.now();
+		allocation.setEndDate(expected);
+
+		LocalDate actual = allocation.getEndDate();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetEndDateAsString() throws Exception {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		Allocation allocation = new Allocation();
+		String expected = LocalDate.now().format(formatter);
+		allocation.setEndDate(LocalDate.now());
+
+		String actual = allocation.getEndDateAsString();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetHours() throws Exception {
+		Allocation allocation = new Allocation();
+		Integer expected = 40;
+		allocation.setHours(expected);
+
+		Integer actual = allocation.getHours();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
 	public void testGetId() throws Exception {
 		Allocation allocation = new Allocation();
 		Long id = new Random().nextLong();
 		allocation.setId(id);
 
 		assertTrue(Long.valueOf(allocation.getId()).equals(id));
+	}
+
+	@Test
+	public void testGetProject() throws Exception {
+		Allocation allocation = new Allocation();
+		Project expected = new Project();
+		expected.setId(1L);
+		expected.setName("Test Project");
+		allocation.setProject(expected);
+
+		Project actual = allocation.getProject();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetResource() throws Exception {
+		Allocation allocation = new Allocation();
+		Resource expected = new Resource();
+		expected.setId(1L);
+		expected.setName("Test Resource");
+		allocation.setResource(expected);
+
+		Resource actual = allocation.getResource();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetSkill() throws Exception {
+		Allocation allocation = new Allocation();
+		Skill expected = new Skill();
+		expected.setId(1L);
+		expected.setName("Test Skill");
+		allocation.setSkill(expected);
+
+		Skill actual = allocation.getSkill();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetStartDate() throws Exception {
+		Allocation allocation = new Allocation();
+		LocalDate expected = LocalDate.now();
+		allocation.setStartDate(expected);
+
+		LocalDate actual = allocation.getStartDate();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testGetStartDateAsString() throws Exception {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		Allocation allocation = new Allocation();
+		String expected = LocalDate.now().format(formatter);
+		allocation.setStartDate(LocalDate.now());
+
+		String actual = allocation.getStartDateAsString();
+
+		assertTrue(expected.equals(actual));
 	}
 
 	@Test
@@ -82,22 +208,96 @@ public class AllocationTest {
 	}
 
 	@Test
+	public void testPreRemove() throws Exception {
+		Allocation expected = new Allocation();
+
+		Skill skill = new Skill();
+		skill.setId(1L);
+		skill.setName("Test Skill");
+		expected.setSkill(skill);
+
+		Resource resource = new Resource();
+		resource.setId(1L);
+		resource.setName("Test Resource");
+		expected.setResource(resource);
+
+		Project project = new Project();
+		project.setId(1L);
+		project.setName("Test Project");
+		expected.setProject(project);
+
+		expected.preRemove();
+
+		Allocation actual = new Allocation();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testSetHours() throws Exception {
+		Allocation allocation = new Allocation();
+		Integer expected = 40;
+		allocation.setHours(expected);
+
+		Integer actual = allocation.getHours();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
 	public void testSetId() throws Exception {
 		Allocation allocation = new Allocation();
 		Long id = new Random().nextLong();
 		allocation.setId(id);
-
 		assertTrue(Long.valueOf(allocation.getId()).equals(id));
 	}
 
 	@Test
-	public void testToString() throws Exception {
+	public void testSetProject() throws Exception {
 		Allocation allocation = new Allocation();
-		allocation.setId(1L);
-		allocation.setHours(40);
-		System.out.println(allocation);
+		Project expected = new Project();
+		expected.setId(1L);
+		expected.setName("Test Project");
+		allocation.setProject(expected);
+
+		Project actual = allocation.getProject();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testSetResource() throws Exception {
+		Allocation allocation = new Allocation();
+		Resource expected = new Resource();
+		expected.setId(1L);
+		expected.setName("Test Resource");
+		allocation.setResource(expected);
+
+		Resource actual = allocation.getResource();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testSetSkill() throws Exception {
+		Allocation allocation = new Allocation();
+		Skill expected = new Skill();
+		expected.setId(1L);
+		expected.setName("Test Skill");
+		allocation.setSkill(expected);
+
+		Skill actual = allocation.getSkill();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void testToString() throws Exception {
+		Allocation allocation =
+			new Allocation(1L, new Project(), new Skill(), LocalDate.now(), LocalDate.now().plusDays(7), 40, new Resource());
 		assertTrue(allocation.toString()
-			.equals("Allocation [id=1, project=null, skill=null, startDate=null, endDate=null, hours=40, resource=null]"));
+			.equals(
+				"Allocation [id=1, project=Project [id=0, name=null], skill=Skill [id=0, name=null], startDate=2019-07-25, endDate=2019-08-01, hours=40, resource=Resource [id=0, name=null, skills=[], allocations=[], hours=0]]"));
 	}
 
 }
--- resourcemanager/src/test/java/com/resourcemanager/model/ResourceTest.java
+++ resourcemanager/src/test/java/com/resourcemanager/model/ResourceTest.java
@@ -2,6 +2,8 @@ package com.resourcemanager.model;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
@@ -9,9 +11,33 @@ import org.junit.Test;
 public class ResourceTest {
 
 	@Test
+	public void testAddAllocation() throws Exception {
+		Resource resource = new Resource();
+		resource.setId(1L);
+		resource.setHours(40);
+		resource.setName("Test Resources");
+		Allocation allocation = new Allocation();
+		allocation.setId(1L);
+		resource.addAllocation(allocation);
+
+		ArrayList<Allocation> expected = new ArrayList<Allocation>();
+		expected.add(allocation);
+		ArrayList<Allocation> actual = (ArrayList<Allocation>) resource.getAllocations();
+
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
 	public void testClone() throws Exception {
 		Resource expected = new Resource();
-		expected.setId(new Random().nextLong());
+		expected.setId(1L);
+		expected.setHours(40);
+		expected.setName("Test Resources");
+		List<Allocation> allocationsExpected = new ArrayList<Allocation>();
+		Allocation allocationExpected = new Allocation();
+		allocationExpected.setId(1L);
+		allocationsExpected.add(allocationExpected);
+		expected.setAllocations(allocationsExpected);
 
 		Resource actual = (Resource) expected.clone();
 		assertTrue(expected.equals(actual));
@@ -22,9 +48,23 @@ public class ResourceTest {
 		Long resourceId = new Random().nextLong();
 		Resource expected = new Resource();
 		expected.setId(resourceId);
+		expected.setHours(40);
+		expected.setName("Test Resources");
+		List<Allocation> allocationsExpected = new ArrayList<Allocation>();
+		Allocation allocationExpected = new Allocation();
+		allocationExpected.setId(1L);
+		allocationsExpected.add(allocationExpected);
+		expected.setAllocations(allocationsExpected);
 
 		Resource actual = new Resource();
 		actual.setId(resourceId);
+		actual.setHours(40);
+		actual.setName("Test Resources");
+		List<Allocation> allocationsActual = new ArrayList<Allocation>();
+		Allocation allocationActual = new Allocation();
+		allocationActual.setId(1L);
+		allocationsActual.add(allocationActual);
+		expected.setAllocations(allocationsActual);
 
 		assertTrue(expected.equals(actual));
 	}
@@ -40,7 +80,6 @@ public class ResourceTest {
 		Resource resource = new Resource();
 		Long id = new Random().nextLong();
 		resource.setId(id);
-
 		assertTrue(Long.valueOf(resource.getId()).equals(id));
 	}
 
@@ -49,25 +88,53 @@ public class ResourceTest {
 		Resource x = new Resource();
 		Resource y = new Resource();
 		x.setId(1L);
-		x.setHours(10);
+		x.setHours(40);
+		x.setName("Test Resources");
+		List<Allocation> allocationsExpected = new ArrayList<Allocation>();
+		Allocation allocationExpected = new Allocation();
+		allocationExpected.setId(1L);
+		allocationsExpected.add(allocationExpected);
+		x.setAllocations(allocationsExpected);
 		y.setId(1L);
-		y.setHours(10);
+		y.setHours(40);
+		y.setName("Test Resources");
+		List<Allocation> allocationsActual = new ArrayList<Allocation>();
+		Allocation allocationActual = new Allocation();
+		allocationActual.setId(1L);
+		allocationsActual.add(allocationActual);
+		y.setAllocations(allocationsActual);
 		assertTrue(x.equals(y) && y.equals(x));
 		assertTrue(x.hashCode() == y.hashCode());
+	}
+
+	@Test
+	public void testRemoveAllocation() throws Exception {
+		Resource resource = new Resource();
+		resource.setId(1L);
+		resource.setHours(40);
+		resource.setName("Test Resources");
+		Allocation allocation = new Allocation();
+		allocation.setId(1L);
+		resource.addAllocation(allocation);
+
+		ArrayList<Allocation> expected = new ArrayList<Allocation>();
+		expected.add(allocation);
+		ArrayList<Allocation> actual = (ArrayList<Allocation>) resource.getAllocations();
+
+		assertTrue(expected.equals(actual));
+
+		expected = new ArrayList<Allocation>();
+		expected.add(allocation);
+		resource.removeAllocation(allocation);
+		actual = (ArrayList<Allocation>) resource.getAllocations();
+
+		assertTrue(expected.equals(actual));
 	}
 
 	@Test
 	public void testResource() throws Exception {
 		Resource resource = new Resource();
 		assertTrue(resource != null);
-	}
-
-	@Test
-	public void testResourceString() throws Exception {
-		Resource resource = new Resource();
-		resource.setId(1L);
-		assertTrue(resource != null);
-		assertTrue(resource.getId() == 1L);
 	}
 
 	@Test
