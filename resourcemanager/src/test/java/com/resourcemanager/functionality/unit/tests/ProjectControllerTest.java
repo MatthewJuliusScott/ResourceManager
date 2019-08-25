aed688c222bd9ecfd25779d2a8156bc629b8611c
Abrody01 <Abrody1188@gmail.com>
2019-08-25 11:59:20 +1000

    Added more tests

--- resourcemanager/src/test/java/com/resourcemanager/functionality/unit/tests/ProjectControllerTest.java
+++ resourcemanager/src/test/java/com/resourcemanager/functionality/unit/tests/ProjectControllerTest.java
@@ -136,4 +136,18 @@ public class ProjectControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(forwardedUrl("/WEB-INF/views/ViewProjects.jsp"));
 	}
+	@Test
+	@WithMockUser(username = "user@gmail.com", password = "password", roles = { "USER", "ADMIN" })
+	public void givenRequestMapping_whenjoinProjects_thenForwardTojoinProjectsJSP()
+		throws Exception {
+
+		mvc.perform(get("/ViewProjects")
+			.contentType(MediaType.TEXT_HTML))
+			.andExpect(status().isOk())
+			.andExpect(forwardedUrl("/WEB-INF/views/ViewProjects.jsp"));
+	}
+	
+	
+	
+	
 }
